ecbbc3cc49f9a00a65006581a695e67d4cd1445e
JeeyaSharma <jeeyas39@gmail.com>
2024-08-23 15:17:23 +0530

    23rd August --- Datatypes and Variables in Js

---
+++ Classwork/Lec-20/data_type.js
@@ -0,0 +1,30 @@
+// 1. Number
+let a=10;
+let b=-5;
+let c=2.5;
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+
+// 2. String
+let name="Jeeya";
+console.log(typeof(name))
+
+// 3. Boolean
+let x=true;
+console.log(typeof(x))
+
+// 4. Undefined
+let y;
+console.log(typeof(y))
+y=20;
+console.log(y)
+console.log(typeof(y))
+
+// 5. Null
+let j=null;
+console.log(typeof(j))
+
+
+// Values of primitive data types cannot be broken down further
+// Values of non-primitive data types can be broken down further
---
+++ Classwork/Lec-20/variables.js
@@ -0,0 +1,29 @@
+//var:
+// var a=10;
+// console.log(a)
+// var a=20;
+// console.log(a)
+// a=30;
+// console.log(a)
+
+
+// Let:
+// let a=10;
+// console.log(a)
+// // let a=20;//redeclaration is not possible when using let
+// a=20;//but it can be updated
+// console.log(a)
+
+
+//const:
+const a=10;
+console.log(a)
+// const a=20;//redeclaration not allowed
+// a=20;//updation of value is also not allowed
+
+
+//update initialize assign -----all mean the same
+
+//var can be redeclared and updated/initialised/assigned value
+//let cannot be redeclared and updated/initialised/assigned value
+//const can neither be redeclared nor updated/initialised/assigned value
