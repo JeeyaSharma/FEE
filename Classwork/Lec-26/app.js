3e9503269a9aed5a670fcd14a3495fe055ff18ba
JeeyaSharma <jeeyas39@gmail.com>
2024-10-07 11:47:41 +0530

    Updated

---
+++ Classwork/Lec-26/app.js
@@ -0,0 +1,10 @@
+// 1. getElementById : id selector
+
+var a=document.getElementById("heading");
+console.log(a);
+console.dir(a);
+a.style.color="blue";
+a.style.background="yellow";
+a.style.border="2px solid red";
+a.style.padding="20px";
+a.style.margin="20px";
